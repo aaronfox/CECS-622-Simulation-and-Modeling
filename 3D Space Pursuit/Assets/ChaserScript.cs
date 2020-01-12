3371de825e79da8237c75516519845096a821c75
aaronfox <aaron.m.fox3@gmail.com>
2020-01-12 12:41:25 -0500

    added arena walls

--- 3D Space Pursuit/Assets/ChaserScript.cs
+++ 3D Space Pursuit/Assets/ChaserScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ChaserScript : MonoBehaviour
 {
+    public Transform escaper; // The escaper's position, angle, and speed
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,12 @@ public class ChaserScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    // Use this function for physics within Unity
+    private void FixedUpdate()
     {
         
     }
