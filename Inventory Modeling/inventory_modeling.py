95f2be0c0d8efd271d3093b5b93d739d7054fcc1
aaronfox <aaron.m.fox3@gmail.com>
2020-01-30 17:07:19 -0500

    Have initial work for setting up inventory model simulation in place

--- Inventory Modeling/inventory_modeling.py
+++ Inventory Modeling/inventory_modeling.py
@@ -1,9 +1,10 @@
 # Aaron Fox
 # CECS 622-01
 # Dr. Adel Elmaghraby
+# Assignment 2 - Problem 2
 
-import sys # For exiting program properly
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLineEdit, QLabel, QHBoxLayout # for GUI
+import sys  # For exiting program properly
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLineEdit, QLabel, QHBoxLayout  # for GUI
 from PyQt5.QtCore import Qt  # To enable maximizing of the GUI
 
 # Use Matplotlib for the embedded graphing display of the dots inside the circle/square
@@ -12,20 +13,17 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches  # For bounding square
 
-import random # For its random.uniform function
-import math # For using square root to find distance points are from circle center
+import random  # For its random.uniform function
+import math  # For using square root to find distance points are from circle center
 
 # All of the gui is based out of this Window class using Python PyQt5 GUI framework
+
+
 class Window(QDialog):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
         # Allow for easy maximizing of the GUI
         self.setWindowFlag(Qt.WindowMaximizeButtonHint, True)
-
-        # For keeping track of points that fall inside circle
-        self.number_of_points_within_cirle = 0
-        # For keeping track of poitns that fall outside the circle
-        self.number_of_points_outside_cirle = 0
 
         # Figure instance for plotting
         self.figure = plt.figure()
@@ -41,7 +39,7 @@ class Window(QDialog):
         self.random_number_input = QLineEdit()
         self.random_number_input_label = QLabel()
         self.random_number_input_label.setText(
-            "Enter N random uniform numbers to generate: ")
+            "Enter Value for Q1: ")
 
         # Button connected to generation of points on figure to visually
         # display the uniform random distribution of points inside/outside the circle
@@ -69,6 +67,10 @@ class Window(QDialog):
 
         self.setLayout(layout)
 
+        # Keep track of values of Q1 and Q2 inventories here
+        self.q1_inventory = 1000
+        self.q2_inventory = 1000
+
         # Create initial empty plot for aesthetic purposes
         self.initial_plot()
 
@@ -78,19 +80,23 @@ class Window(QDialog):
 
         # create an axis
         ax = self.figure.add_subplot(1, 1, 1)
-        plt.gca().set_aspect('equal', adjustable='box')
+        # plt.gca().set_aspect('equal', adjustable='box')
 
-        # Create a circle patch
-        circle = patches.Circle(xy=(.5, .5), radius=.5,
-                                linewidth=1, edgecolor='r', facecolor='none')
+        labels = ['Q1', 'Q2']
+        label_locs = [1, 2]
+        width = 0.35
+        ax.bar(label_locs, [self.q1_inventory, self.q2_inventory], width, label='Q1')
 
-        # Add the patch to the Axes
-        ax.add_patch(circle)
+        # Set Graph labels, title, x-axis
+        ax.set_ylabel('Inventory Available')
+        ax.set_title('Inventory for Store')
+        ax.set_xticks(label_locs)
+        ax.set_xticklabels(labels)
 
         # refresh canvas
         self.canvas.draw()
 
-    # Generates and displays the circle, graph, and the uniform random points on the graph 
+    # Generates and displays the circle, graph, and the uniform random points on the graph
     def generate(self):
         # Clear the figure in case stuff is on it already
         self.figure.clear()
@@ -126,11 +132,11 @@ class Window(QDialog):
             for i in range(len(uniform_numbers)):
                 uniform_number_x.append(uniform_numbers[i][0])
                 uniform_number_y.append(uniform_numbers[i][1])
-                # If the distance of the point from the circle is less than or equal to 0.5, 
+                # If the distance of the point from the circle is less than or equal to 0.5,
                 # then that point falls within the circle
                 # Accoount for the fact that the center of the circle is at (0.5, 0.5), so distance
                 # formula for distance from center is sqrt((x - 0.5)^2 + (y - 0.5)^2)
-                if math.sqrt((uniform_numbers[i][0] - 0.5)** 2 + (uniform_numbers[i][1] - 0.5) ** 2) <= 0.5:
+                if math.sqrt((uniform_numbers[i][0] - 0.5) ** 2 + (uniform_numbers[i][1] - 0.5) ** 2) <= 0.5:
                     number_points_inside = number_points_inside + 1
 
             print("number_points_inside == " + str(number_points_inside))
@@ -145,21 +151,13 @@ class Window(QDialog):
             # 4 means that it should yield an estimated value of pi
             estimated_value = 4 * (number_points_inside / len(uniform_numbers))
             # Update estimated pi value label
-            self.pi_estimate_text.setText("Estimated value of pi: " + str(estimated_value))
+            self.pi_estimate_text.setText(
+                "Estimated value of pi: " + str(estimated_value))
 
             # refresh canvas
             self.canvas.draw()
         else:
             print("Input does not contain a valid number")
-
-    # Generates N uniform random numbers 
-    def generate_uniform_numbers(self, N):
-        uniform_numbers = []
-        for i in range(0, N):
-            uniform_numbers.append(
-                [random.uniform(0, 1), random.uniform(0, 1)])
-        return uniform_numbers
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
