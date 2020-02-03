ac337c82b05f56c2c31a37a53c6638fd54981dd2
aaronfox <aaron.m.fox3@gmail.com>
2020-02-03 14:10:21 -0500

    Figuring out why prev/next day methods aren't working >:(

--- Inventory Modeling/inventory_modeling.py
+++ Inventory Modeling/inventory_modeling.py
@@ -15,9 +15,11 @@ import matplotlib.patches as patches  # For bounding square
 
 import random  # For its random.uniform function
 import math  # For using square root to find distance points are from circle center
-import numpy as np # For the use of a Gaussian distribution of the customer purchases
+import numpy as np  # For the use of a Gaussian distribution of the customer purchases
 
 # All of the gui is based out of this Window class using Python PyQt5 GUI framework
+
+
 class Window(QDialog):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
@@ -43,7 +45,7 @@ class Window(QDialog):
 
         # Q2 Input and Label
         self.Q2 = QLineEdit()
-        self.Q2.setText("1000")
+        self.Q2.setText("1200")
         self.Q2_label = QLabel()
         self.Q2_label.setText("Q2: ")
 
@@ -71,10 +73,26 @@ class Window(QDialog):
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
 
-        # Estimate of pi value
-        self.pi_estimate_text = QLabel()
-        self.pi_estimate_text.setText("Estimated value of pi: ")
-        layout.addWidget(self.pi_estimate_text)
+        # TEST
+        self.day_to_evaluate_value = QLineEdit()
+        self.day_to_evaluate_value.setText("1")
+        self.day_to_evaluate_label = QLabel()
+        self.day_to_evaluate_label.setText("Enter Day to Evaluate: ")
+
+        self.next_day_button = QPushButton("Next Day")
+        self.next_day_button.clicked.connect(self.next_day_clicked)
+
+        self.prev_day_button = QPushButton("Prev Day")
+        self.next_day_button.clicked.connect(self.prev_day_clicked)
+
+        horizontal_layout1 = QHBoxLayout()
+        horizontal_layout1.addWidget(self.day_to_evaluate_label)
+        horizontal_layout1.addWidget(self.day_to_evaluate_value)
+        horizontal_layout1.addWidget(self.prev_day_button)
+        horizontal_layout1.addWidget(self.next_day_button)
+        layout.addLayout(horizontal_layout1)
+
+        # END TEST
 
         # Number of N numbers to generate input
         horizontal_layout = QHBoxLayout()
@@ -106,21 +124,131 @@ class Window(QDialog):
         self.P1_purchase_request_wait_time = random.randint(1, 4)
         self.P2_purchase_request_wait_time = random.randint(1, 4)
 
+        # Place all simulation results here
+        self.simulation_inventory_results = []
+
         # Create initial empty plot for aesthetic purposes
         self.initial_plot()
 
-    # Initial plot to make the GUI look initially pretty for aesthetic purposes
-    def initial_plot(self):
+        # Second Figure for line plot
+        # Figure instance for plotting
+        self.figure2 = plt.figure()
+
+        # self.canvas is the widget that displays the figure,
+        # taking the figure instance as a parameter to __init__
+        self.canvas2 = FigureCanvas(self.figure2)
+        layout.addWidget(self.canvas2)
+        self.lineplot_init()
+
+    # Plot next day clicked
+    def next_day_clicked(self):
         self.figure.clear()
 
         # create an axis
         ax = self.figure.add_subplot(1, 1, 1)
         # plt.gca().set_aspect('equal', adjustable='box')
 
+        self.current_day = self.current_day + 1
+
         labels = ['Q1', 'Q2']
         label_locs = [1, 2]
         width = .40
-        ax.bar(label_locs, [self.Q1_inventory, self.Q2_inventory], width, label='Q1')
+        # ax.bar(label_locs, [self.simulation_inventory_results[self.current_day][0], self.simulation_inventory_results[self.current_day][1]], width, label='Q1')
+        ax.bar(label_locs, [10, 11], width, label='Q1')
+    
+
+        print('test')
+        # Set Graph labels, title, x-axis
+        ax.set_ylabel('Inventoryxx Available')
+        ax.set_title('Inventory for Store')
+        ax.set_xticks(label_locs)
+        ax.set_xticklabels(labels)
+
+        # refresh canvas
+        self.canvas.draw()
+
+    # Plot prev day clicked
+    def prev_day_clicked(self):
+        self.figure.clear()
+
+        # create an axis
+        ax=self.figure.add_subplot(1, 1, 1)
+        # plt.gca().set_aspect('equal', adjustable='box')
+
+        if self.current_day > 0:
+            self.current_day=self.current_day - 1
+
+        labels=['Q1', 'Q2']
+        label_locs=[1, 2]
+        width=.40
+        ax.bar(label_locs, [self.simulation_inventory_results[self.current_day][0],
+                            self.simulation_inventory_results[self.current_day][1]], width, label='Q1')
+
+        # Set Graph labels, title, x-axis
+        ax.set_ylabel('Inventory Available')
+        ax.set_title('Inventory for Store')
+        ax.set_xticks(label_locs)
+        ax.set_xticklabels(labels)
+
+        # refresh canvas
+        self.canvas.draw()
+
+    # Initial Line plot to make GUI look initially pretty for aesthetic purpises
+    def lineplot_init(self):
+        self.figure2.clear()
+        # create an axis
+        ax=self.figure2.add_subplot(1, 1, 1)
+
+        # Set Graph labels, title, x-axis
+        ax.set_ylabel('Inventory Available')
+        ax.set_xlabel('Day')
+        ax.set_title('Inventory for Store')
+
+    def plot_line_graph(self):
+        self.figure2.clear()
+        # create an axis
+        ax=self.figure2.add_subplot(1, 1, 1)
+
+        # Set Graph labels, title, x-axis
+        ax.set_ylabel('Inventory Available')
+        ax.set_xlabel('Day')
+        ax.set_title('Inventory for Store')
+
+        Q1_nums=[]
+        Q2_nums=[]
+        for i in range(len(self.simulation_inventory_results)):
+            Q1_nums.append(self.simulation_inventory_results[i][0])
+            Q2_nums.append(self.simulation_inventory_results[i][1])
+
+        print("Q1_nums == " + str(Q1_nums))
+        print("y_nums == " + str(Q2_nums))
+
+        print("range(len(Q1_nums) == " + str(range(len(Q1_nums))))
+        Q1_line=ax.plot(range(len(Q1_nums)), Q1_nums)
+        Q2_line=ax.plot(range(len(Q2_nums)), Q2_nums)
+
+        ax.legend((Q1_line, Q2_line), ('Q1 Inventory',
+                  'Q2 Inventory'), loc='upper right')
+
+        # refresh canvas
+        self.canvas2.draw()
+
+        print("hi")
+
+
+    # Initial plot to make the GUI look initially pretty for aesthetic purposes
+    def initial_plot(self):
+        self.figure.clear()
+
+        # create an axis
+        ax=self.figure.add_subplot(1, 1, 1)
+        # plt.gca().set_aspect('equal', adjustable='box')
+
+        labels=['Q1', 'Q2']
+        label_locs=[1, 2]
+        width=.40
+        ax.bar(label_locs, [self.Q1_inventory,
+               self.Q2_inventory], width, label='Q1')
 
         # Set Graph labels, title, x-axis
         ax.set_ylabel('Inventory Available')
@@ -136,97 +264,108 @@ class Window(QDialog):
 
         # START Run all of simulation in this block
         # Create array of days up to 50 and then display results
-        # Initial inventory 
-        Q1_inventory_remaining = self.Q1_inventory
-        Q2_inventory_remaining = self.Q2_inventory
-        number_of_days_to_run_simulation = 50
+        # Initial inventory
+        Q1_inventory_remaining=self.Q1_inventory
+        Q2_inventory_remaining=self.Q2_inventory
+        number_of_days_to_run_simulation=50
 
-        # Create list of inventory for every day 
-        inventories = [[Q1_inventory_remaining, Q2_inventory_remaining]]
-        temp_purchase_request_times = [[self.P1_purchase_request_wait_time, self.P2_purchase_request_wait_time]]
-
+        # Create list of inventory for every day
+        inventories=[[Q1_inventory_remaining, Q2_inventory_remaining]]
+        temp_purchase_request_times=[
+            [self.P1_purchase_request_wait_time, self.P2_purchase_request_wait_time]]
+        temp_purchased_amounts=[]
         # Run simulation for the input number of dats to run the simulation
         for i in range(number_of_days_to_run_simulation):
             if self.P1_purchase_request_wait_time == 0:
-                Q1_inventory_remaining = Q1_inventory_remaining + self.P1_request
-                self.P1_purchase_request_wait_time = random.randint(1, 4)
+                Q1_inventory_remaining=Q1_inventory_remaining + self.P1_request
+                self.P1_purchase_request_wait_time=random.randint(1, 4)
             else:
-                self.P1_purchase_request_wait_time = self.P1_purchase_request_wait_time - 1
+                self.P1_purchase_request_wait_time=self.P1_purchase_request_wait_time - 1
 
             if self.P2_purchase_request_wait_time == 0:
-                Q2_inventory_remaining = Q2_inventory_remaining + self.P2_request
-                self.P2_purchase_request_wait_time = random.randint(1, 4)
+                Q2_inventory_remaining=Q2_inventory_remaining + self.P2_request
+                self.P2_purchase_request_wait_time=random.randint(1, 4)
             else:
-                self.P2_purchase_request_wait_time = self.P2_purchase_request_wait_time - 1
+                self.P2_purchase_request_wait_time=self.P2_purchase_request_wait_time - 1
 
             # Every day, have customers purchase a certain amount of inventory from business
-            # Assume customers can purchase up 
+            # Assume customers can purchase up
             # Mean value of days of inventory bought of the original stock value
-            mean_days_of_stock_purchased_by_customers = 25
+            mean_days_of_stock_purchased_by_customers=90
             # mean and standard deviation, respectively
-            mu, sigma = self.Q1_inventory/mean_days_of_stock_purchased_by_customers, 100.5
-            distribution_set = np.random.normal(mu, sigma, 1000)
+            mu, sigma=self.Q1_inventory/mean_days_of_stock_purchased_by_customers, 100.5
+            distribution_set=np.random.normal(mu, sigma, 1000)
 
             # Ensure randomly chosen number is not negative (very unlikely given the
             # Gaussian distribution, but still possible)
-            i = 0
-            normal_random_q1 = distribution_set[i]
+            i=0
+            normal_random_q1=distribution_set[i]
             while normal_random_q1 < 1:
-                i = i + 1
-                normal_random_q1 = distribution_set[i]
+                i=i + 1
+                normal_random_q1=distribution_set[i]
 
             # Ensure independency of values of Q1 and Q2 by making sure they use two separate distributions
 
             # Mean value of days of inventory bought of the original stock value
-            mean_days_of_stock_purchased_by_customers_q2 = 25
+            mean_days_of_stock_purchased_by_customers_q2=90
             # mean and standard deviation, respectively
-            mu, sigma = self.Q2_inventory/mean_days_of_stock_purchased_by_customers_q2, 100.5
-            distribution_set_q2 = np.random.normal(mu, sigma, 1000)
+            mu, sigma=self.Q2_inventory/mean_days_of_stock_purchased_by_customers_q2, 100.5
+            distribution_set_q2=np.random.normal(mu, sigma, 1000)
 
-            i = 0
-            normal_random_q2 = distribution_set_q2[i]
+            i=0
+            normal_random_q2=distribution_set_q2[i]
             while normal_random_q2 < 1:
-                i = i + 1
-                normal_random_q2 = distribution_set_q2[i]
+                i=i + 1
+                normal_random_q2=distribution_set_q2[i]
+
+            temp_purchased_amounts.append([normal_random_q1, normal_random_q2])
 
             # Decrement respective amounts of stock according to these two independent Gaussian distribution selections
-            Q1_inventory_remaining = Q1_inventory_remaining - math.ceil(normal_random_q1)
-            Q2_inventory_remaining = Q2_inventory_remaining - math.ceil(normal_random_q2)
+            Q1_inventory_remaining=Q1_inventory_remaining - \
+                math.ceil(normal_random_q1)
+            Q2_inventory_remaining=Q2_inventory_remaining - \
+                math.ceil(normal_random_q2)
 
             # Ensure quantities do not go into the negatives
             if Q1_inventory_remaining < 0:
-                Q1_inventory_remaining = 0
+                Q1_inventory_remaining=0
 
             if Q2_inventory_remaining < 0:
-                Q2_inventory_remaining = 0
+                Q2_inventory_remaining=0
 
-            
-            temp_purchase_request_times.append([self.P1_purchase_request_wait_time, self.P2_purchase_request_wait_time])
-            inventories.append([Q1_inventory_remaining, Q2_inventory_remaining])
+
+            temp_purchase_request_times.append(
+                [self.P1_purchase_request_wait_time, self.P2_purchase_request_wait_time])
+            inventories.append(
+                [Q1_inventory_remaining, Q2_inventory_remaining])
         # END Run of all simulation
-        print(inventories)
+        self.simulation_inventory_results=inventories
+        print(self.simulation_inventory_results)
+        self.plot_line_graph()
+
+        # print(temp_purchased_amounts)
         # print(temp_purchase_request_times)
         # Clear the figure in case stuff is on it already
         self.figure.clear()
 
         # create an axis
-        ax = self.figure.add_subplot(1, 1, 1)
+        ax=self.figure.add_subplot(1, 1, 1)
 
         # Get input N (amount of numbers to generate) from input
         # Make sure the input is a number and is not 0 (to prevent divide by 0 error)
-        if (self.Q1.text().isnumeric() and int(self.Q1.text()) != 0 
+        if (self.Q1.text().isnumeric() and int(self.Q1.text()) != 0
             and self.Q2.text().isnumeric() and int(self.Q2.text()) != 0
             and self.P1.text().isnumeric() and int(self.P1.text()) != 0
             and self.P2.text().isnumeric() and int(self.P2.text()) != 0):
-            self.Q1_inventory = int(self.Q1.text())
-            self.Q2_inventory = int(self.Q2.text())
-            self.P1_request = int(self.P1.text())
-            self.P2_request = int(self.P2.text())
+            self.Q1_inventory=int(self.Q1.text())
+            self.Q2_inventory=int(self.Q2.text())
+            self.P1_request=int(self.P1.text())
+            self.P2_request=int(self.P2.text())
 
             # Prep basics of bar graph
-            labels = ['Q1', 'Q2']
-            label_locs = [1, 2]
-            width = .40
+            labels=['Q1', 'Q2']
+            label_locs=[1, 2]
+            width=.40
             ax.bar(label_locs, [self.Q1_inventory,
                                 self.Q2_inventory], width, label='Q1')
 
@@ -236,16 +375,16 @@ class Window(QDialog):
             ax.set_xticks(label_locs)
             ax.set_xticklabels(labels)
 
-            
+
             # refresh canvas
             self.canvas.draw()
         else:
             print("Inputs do not contain a valid number")
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app=QApplication(sys.argv)
 
-    main = Window()
+    main=Window()
     main.show()
 
     sys.exit(app.exec_())
