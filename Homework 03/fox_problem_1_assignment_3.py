f67ff5c97a933deb0713945d9414f132d7bad546
aaronfox <aaron.m.fox3@gmail.com>
2020-02-22 16:07:35 -0500

    initial homework 3 commit

---
+++ Homework 03/fox_problem_1_assignment_3.py
@@ -0,0 +1,14 @@
+# Aaron Fox
+# CECS 622-01
+# Dr.  Elmaghraby
+# Assignment 3 Problem 1
+
+class tank:
+    def __init__(self, height_1, area_1, flow_1, height_2, area_2, flow_2):
+        pass
+    
+    def print(self):
+        print("hi")
+
+if __name__ == "__main__":
+    print("Running Assignment 3 Problem 1")
