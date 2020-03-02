307f4953665b45c5b3f0cd87abab96a06f77a5a0
aaronfox <aaron.m.fox3@gmail.com>
2020-03-02 11:53:17 -0500

    Got recursion working alright

--- Homework 03/critical_path_simulation.py
+++ Homework 03/critical_path_simulation.py
@@ -23,8 +23,19 @@ class Node:
     def get_connecting_nodes(self):
         return self.node_connections
 
-    # def print(self):
-    #     print(self.node_connections)
+
+def analyze_performance(start_node, end_node, curr_list):
+    # print(start_node)
+    if start_node == end_node:
+        curr_list.append(start_node)
+        print("curr_list == " + str(curr_list))
+        print("FINISHED")
+        return
+    for node, node_length in start_node.get_connecting_nodes():
+        if start_node not in curr_list:
+            curr_list.append(start_node)
+        copy_list = curr_list.copy()
+        analyze_performance(node, end_node, copy_list)
 
 if __name__ == "__main__":
     print("Running critical path simulation...")
@@ -36,9 +47,27 @@ if __name__ == "__main__":
     node_6 = Node("Node 6")
     node_7 = Node("Node 7")
 
+    # Add respective deterministic/uniformly distributed connections per prompt
     ## Node 1 ##
     node_1.add_connection(node_2, random.uniform(4, 6))  # (1, 2): U (4,6)
     node_1.add_connection(node_5, 6)  # (1, 5): 6
-    print(node_1)
 
     ## Node 2 ##
+    node_2.add_connection(node_3, 6)  # (2, 3): 6
+    node_2.add_connection(node_4, random.uniform(6, 8))  # (2, 4): U (6,8)
+
+    ## Node 3 ##
+    node_3.add_connection(node_4, random.uniform(4, 8))  # (3, 4): U (4,8)
+
+    ## Node 4 ##
+    node_4.add_connection(node_7, 4)  # (4, 7): 4
+
+    ## Node 5 ##
+    node_5.add_connection(node_3, 8)  # (5, 3): 8
+    node_5.add_connection(node_4, 11)  # (5, 4): 11
+    node_5.add_connection(node_6, random.uniform(8, 10))  # (5, 6): U (8,10)
+
+    ## Node 5 ##
+    node_6.add_connection(node_7, random.uniform(9, 10))  # (6, 7): U (9, 10)
+
+    analyze_performance(start_node=node_1, end_node=node_7, curr_list=[])
