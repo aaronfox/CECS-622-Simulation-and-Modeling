b3ab7353b3fc52ad2d68bc222c25dee06c381ccd
aaronfox <aaron.m.fox3@gmail.com>
2020-04-01 00:47:10 -0400

    Progress toward sim up to likelihood of spreading the rumor

--- Test 1 (Homework 04)/Fox_Test_1.py
+++ Test 1 (Homework 04)/Fox_Test_1.py
@@ -3,6 +3,10 @@
 # Dr. Elmaghraby
 # Test 1 (Rumors Simulation Project)
 
+# Used for selecting a random starting student to start spreading the rumor
+# and for shuffling students to later pair them
+import random # Using randint and shuffle
+
 ## Story ##
 # + N students are at a party (N is an even integer 2 ≤ N ≤ 10,000)
 # + At some point, all students pair off at random and talk for exactly one minute.
@@ -28,6 +32,11 @@ class Student:
         # each student has talked to
         self.student_id = student_id
 
+        # Keeps track of who has heard the rumor. This is important because the very
+        # first student knows the rumor but has not heard it from anyone at the party yet, so technically
+        # their times_heard_rumor is 0
+        self.has_heard_rumor = False
+
         # The number of times that the student has heard the rumor.
         # If this is 0, then the student has not heard the rumor.
         # If this is 2, then the student stops spreading the rumor
@@ -41,3 +50,79 @@ class Student:
         # person twice and increase another student's times_heard_rumor twice
         self.students_spread_rumor_to = []
 
+    def __repr__(self):
+        return "Student ID: " + str(self.student_id)
+
+    def __str__(self):
+        return str("Student ID: " + str(self.student_id) + ", Has heard rumor: "\
+            + str(self.has_heard_rumor) + ", Number of times heard rumor: " + str(self.times_heard_rumor)\
+            + ", Likelihood of spreading rumor: " + str(self.likelihood_of_spreading)\
+            + ", Students spread rumor to: " + str(self.students_spread_rumor_to))
+
+    def set_heard_rumor(self, has_heard_rumor):
+        self.has_heard_rumor = has_heard_rumor
+
+    def get_id(self):
+        return self.student_id
+
+    def append_students_spread_rumor_to(self, other_student_id):
+        self.students_spread_rumor_to.append(other_student_id)
+
+    def increment_times_heard_rumor(self):
+        # Ensure set heard rumor is true for the student
+        self.set_heard_rumor(True)
+        self.times_heard_rumor = self.times_heard_rumor + 1
+
+    def get_times_heard_rumor(self):
+        return self.times_heard_rumor
+
+def run_rumor_simulation(likelihood_of_spreading_rumor=0.5, number_of_students=10, minutes_to_run=10):
+    if number_of_students % 2 != 0:
+        print("Error: Input parameter number_of_students must be even. " + str(number_of_students) + " is not an even number.")
+        return
+
+    students = []
+    # First, create an array of students
+    for i in range(number_of_students):
+        students.append(Student(student_id=i, times_heard_rumor=0, likelihood_of_spreading=likelihood_of_spreading_rumor))
+
+    # Randomly set one student to have heard the rumor by setting times
+    student_to_start_rumor = random.randint(0,number_of_students - 1)
+    students[student_to_start_rumor].set_heard_rumor(True)
+
+    # Run over every minute needed in given parameter minutes_to_run
+    for minute in range(minutes_to_run):
+        # Get random pairs of each student by randomly shuffling everybody and then pairing them into twos
+        random.shuffle(students)
+        print("students == " + str(students))
+        # Iterate over every pair and evaluate results
+        for i in range(0, number_of_students, 2):
+            print("Pairing " + repr(students[i]) + " with " + repr(students[i + 1]))
+            # Since initial student initially spreads rumor to first conversion partner at minute 0, make sure this happens here
+            if minute == 0 and (students[i].get_id() == student_to_start_rumor or students[i + 1].get_id() == student_to_start_rumor):
+                # Check first case where current even number student in shuffled students list is initial rumor spreader
+                if students[i].get_id() == student_to_start_rumor:
+                    students[i].append_students_spread_rumor_to(students[i + 1].get_id())
+                    students[i + 1].increment_times_heard_rumor()
+                    print("First spread from initial " + repr(students[i]) + " to " + repr(students[i + 1]))
+                # Else, it's second case where current odd number student in shuffled students list is initial rumor spreader
+                else:
+                    students[i + 1].append_students_spread_rumor_to(students[i].get_id())
+                    students[i].increment_times_heard_rumor()
+                    print("First spread from initial " + repr(students[i + 1]) + " to " + repr(students[i]))
+            else:
+                # Check if anyone in pair has heard rumor. If not, then skip pair
+                if students[i].has_heard_rumor or students[i + 1].has_heard_rumor:
+                    # Check if randomly generated float is less than likelihood of spreading rumor. If so, spread rumor
+                    # If true, spread rumor to student who has not heard rumor
+                    if random.random() < likelihood_of_spreading_rumor: 
+                        
+
+
+
+    for s in students:
+        print(s)
+
+
+
+run_rumor_simulation(likelihood_of_spreading_rumor=0.5, number_of_students=10, minutes_to_run=10)
