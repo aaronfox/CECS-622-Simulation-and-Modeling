d16e9fa3f967d3c2d16e5b15f80c7407fe38e588
aaronfox <aaron.m.fox3@gmail.com>
2020-04-17 00:14:07 -0400

    Sim looking freeeesh

--- Final Project/Fox_Final_Project_Elevator.py
+++ Final Project/Fox_Final_Project_Elevator.py
@@ -24,6 +24,9 @@ class Person:
         self.departure_time = 0
         self.set_arrival_time(arrival_time)
 
+        # Identify each person in order of who got off first
+        self.id = 0
+
     # Sets the arrival time of the person to the ground floor elevator (either line or no line)
     def set_arrival_time(self, arrival_time):
         self.arrival_time = arrival_time
@@ -35,6 +38,16 @@ class Person:
 
     def get_floor(self):
         return self.floor_to_go_to
+
+    def set_id(self, id):
+        self.id = id
+
+    def __str__(self):
+        return "[Person #" + str(self.id) + ", Arrival Time: " + str(self.arrival_time) + ", Departure Time: " + str(self.departure_time) + ", Wait Time: " + str(self.wait_time) + "]\n"
+
+
+    def __repr__(self):
+        return "[Person #" + str(self.id) + ", Arrival Time: " + str(self.arrival_time) + ", Departure Time: " + str(self.departure_time) + ", Wait Time: " + str(self.wait_time) + "]"
 
 def run_elevator_simulation(mean_interarrival_time):
     # Generate times for people to arrive at elevator until time reaches 61 (which is 9:00)
@@ -72,14 +85,16 @@ def run_elevator_simulation(mean_interarrival_time):
     current_time = 0
     next_time = times_to_arrive[i]
     i = i + 1
+    all_persons = []
+    id_num = 1
     # Run loop to iterate over needed hours 08:00-09:00+ (0 = 08:00, ..., 59 = 08:59, 60 = 9:00, etc.)
     # Continue running loop until all people have joined, the queue is empty, and all elevator_occupants have departed the lift
     while i < len(times_to_arrive) - 1 or len(queue) != 0 or len(elevator_occupants) != 0:
-        # TODO: use the variable has_time_moved_forward to check and make sure that time has moved forward. If not, increment by an amount
+        # Use the variable has_time_moved_forward to check and make sure that time has moved forward. If not, increment by an amount
         has_time_moved_forward = False
-        print("Length of queue == " + str(len(queue)))
-        print("Length of elevator_occupants == " + str(len(elevator_occupants)))
-        print("Current Time: " + str(current_time))
+        # print("Length of queue == " + str(len(queue)))
+        # print("Length of elevator_occupants == " + str(len(elevator_occupants)))
+        # print("Current Time: " + str(current_time))
 
         # Always start on ground floor (floor 0)
         elevator_floor = 1
@@ -112,20 +127,20 @@ def run_elevator_simulation(mean_interarrival_time):
             if len(elevator_occupants) < 12:
                 # Add persons from queue to elevator
                 while len(elevator_occupants) < 12 and len(queue) >= 1:
-                    print("adding from queue to elevator")
                     person_to_add = queue[0]
                     elevator_occupants.append(person_to_add)
                     del queue[0]
-                    print("test: " + str(person_to_add.get_floor()))
 
             # Check if any occupant is going to second floor
             elevator_floor = 2
             someone_got_off_2nd = False
             for occupant in elevator_occupants:
                 if occupant.get_floor() == 2:
-                    # TODO: Get passengers off here and add data to add
-                    print("going to second")
                     someone_got_off_2nd = True
+                    occupant.set_id(id_num)
+                    id_num = id_num + 1
+                    occupant.set_departure_time(current_time)
+                    all_persons.append(occupant)
                     elevator_occupants.remove(occupant)
             
             if someone_got_off_2nd:
@@ -141,9 +156,11 @@ def run_elevator_simulation(mean_interarrival_time):
             someone_got_off_3rd = False
             for occupant in elevator_occupants:
                 if occupant.get_floor() == 3:
-                    # TODO: Get passengers off here and add data to add
-                    print("going to third")
                     someone_got_off_3rd = True
+                    occupant.set_id(id_num)
+                    id_num = id_num + 1
+                    occupant.set_departure_time(current_time)
+                    all_persons.append(occupant)
                     elevator_occupants.remove(occupant)
             
             # See where elevator came from
@@ -163,13 +180,15 @@ def run_elevator_simulation(mean_interarrival_time):
                     # Opening elevator door takes 0.5 minutes
                     current_time = current_time + 0.5
 
-            # Check if any occupant is going to second floor
+            # Check if any occupant is going to fourth floor
             elevator_floor = 4
             someone_got_off_4th = False
             for occupant in elevator_occupants:
                 if occupant.get_floor() == 4:
-                    # TODO: Get passengers off here and add data to add
-                    print("going to fourth")
+                    occupant.set_id(id_num)
+                    id_num = id_num + 1
+                    all_persons.append(occupant)
+                    occupant.set_departure_time(current_time)
                     elevator_occupants.remove(occupant)
                     someone_got_off_4th = True
 
@@ -194,11 +213,19 @@ def run_elevator_simulation(mean_interarrival_time):
                     # Opening elevator door takes 0.5 minutes
                     current_time = current_time + 0.5
 
-        # TODO: implement this
+        # Ensure time always moves forward to avoid stasis
         if has_time_moved_forward == False:
-            # print("forcing time forward")
-            # Edit this number by realizing that -ln(0.99) / 6 = 0.001675
-            current_time = current_time + 0.001675
+            # Since -ln(0.999999) / 6 = 0.000001675, it is very, very, very unlikely 
+            # (re: practically impossible) that a person's intial term would skip over their time
+            # at the very same time they arrived
+            current_time = current_time + 0.000000166667
+
+    print("All Persons: " + str(all_persons))
+    # Write data to text file
+    f = open(r'C:\Users\aaron\Classes_11th_Semester\CECS 622\CECS-622-Simulation-and-Modeling\Final Project\data_results.txt', "w")
+    for person in all_persons:
+        f.write(str(person))
+    f.close()
 
 if __name__ == "__main__":
     mean_interarrival_time = 0.1667
